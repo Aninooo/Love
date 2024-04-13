f4af4fa3172e279fd708510c6adba320dcebdf03
Bryan N. Lomerio <102351612+Aninooo@users.noreply.github.com>
2024-04-13 15:54:19 +0800

    Update FrontPage.jsx

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,30 +1,13 @@
-// FrontPage.jsx
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import "../index.css";
 
 const FrontPage = ({ goToGallery }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    document.body.classList.toggle("dark-mode", !isDarkMode);
-    document.body.classList.toggle("light-mode", isDarkMode);
-  };
-
   return (
-    <div className={`container ${isDarkMode ? "dark-mode" : ""}`}>
+    <div className="container">
       <h1 className="title">Love Captured: A Gallery of Moments</h1>
       <button className="button" onClick={goToGallery}>
         View Gallery
-      </button>
-      <button className="dark-mode-button" onClick={toggleDarkMode}>
-        <FontAwesomeIcon
-          icon={isDarkMode ? faSun : faMoon}
-          className="dark-mode-icon"
-        />
       </button>
     </div>
   );
